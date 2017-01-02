a91b55a09426f669c5bf5dfcf8ba0e270ef21bca
sagiv562 <sagiviphone4s@gmail.com>
2017-01-02 18:47:55 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var path = require('path');
+
+var passport = require('passport');
+var flash = require('connect-flash');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var requestHandler = require('./controlars/requestHandler');
+
+
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// send the app to the request handler
+requestHandler(app, bodyParser);
+
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// My error handler
+
+
+// express error handler for the developer
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
+
+
+
